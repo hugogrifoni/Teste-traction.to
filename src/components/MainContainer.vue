82a549a00ef84397f81044de6ff001b9f5ed0b3c
Hugo Grifoni <hugobgrifoni@gmail.com>
2023-01-20 10:12:39 -0300

    updates and remove unused code

--- src/components/MainContainer.vue
+++ src/components/MainContainer.vue
@@ -1,6 +1,7 @@
 <script setup>
 import NotFavoriteIcon from './icons/IconNotFavorite.vue';
 import FavoriteIcon from './icons/IconFavorite.vue';
+import MessageItem from './MessageItem.vue';
 </script>
 
 <script>
@@ -14,7 +15,7 @@ export default {
 	},
 	methods: {
 		addPost() {
-			if (this.name.trim() === '' || this.message.trim() === '') {
+			if (this.message.trim() === '') {
 				return;
 			}
 			this.posts.push({
@@ -22,7 +23,6 @@ export default {
 				message: this.message,
 				isFavorite: false,
 			});
-			this.name = '';
 			this.message = '';
 		},
 		toggleFavoritePost(index) {
@@ -34,66 +34,45 @@ export default {
 
 <template>
 	<div
-		class="flex flex-col items-center bg-slate-700 text-white h-screen bg-[url('../assets/image-bg.png')]"
+		class="flex flex-col items-center bg-slate-700 text-white pb-10 min-h-screen bg-[url('../assets/image-bg.png')]"
 	>
 		<div class="my-8">
 			<img src="../assets/logo.png" />
 		</div>
 		<hr />
-		<h2 class="py-5">Social Feed</h2>
+		<h2 class="py-5 text-3xl font-bold mt-5">Social Feed</h2>
 
-		<div class="w-2/3">
-			<div>
-				<input
-					class="text-black w-full mb-2 p-2 rounded-lg"
-					placeholder="Nome"
-					type="text"
-					name="author"
-					v-model="name"
-				/>
-			</div>
-			<div>
+		<div class="flex flex-col w-2/3 rounded-xl bg-neutral-900">
+			<div class="p-3">
 				<textarea
-					class="text-black w-full mb-2 p-2 rounded-lg"
+					class="text-white resize-none h-28 w-full mb-0 p-2 bg-neutral-900"
 					placeholder="Insira sua mensagem..."
 					type="text"
 					name="author"
 					v-model="message"
 				/>
 			</div>
-			<div
-				class="box-border border-2 border-lime-600 rounded-lg w-14 text-center content-end bg-lime-600 text-white"
-			>
+			<div class="w-full border-t border-gray-700" />
+			<div class="ml-auto p-4">
 				<button
-					class="btn btn-primary"
+					class="w-36 text-center rounded-lg py-1 border-lime-600 bg-lime-600 text-white"
 					v-on:click="addPost"
 					type="submit"
 				>
-					post
+					Postar
 				</button>
 			</div>
 		</div>
 
-		<div class="w-2/3 py-12 text-gray-400">
-			<div
-				class="box-border border-2 my-5 py-5 px-2.5 rounded-lg"
-				v-for="(comment, index) in posts"
-			>
-				<span class="postAuthor"
-					>Autor: <strong>{{ comment.name }}</strong></span
-				>
-				<p>{{ comment.message }}</p>
+		<h2 class="text-3xl font-bold mt-10">Últimos posts</h2>
 
-				<a
-					href="#"
-					title="favoritar"
-					v-on:click="toggleFavoritePost(index)"
-				>
-					<div class="flex items-center" v-if="!comment.isFavorite">
-						<FavoriteIcon /> Desfavoritar
-					</div>
-					<div class="flex" v-else><NotFavoriteIcon /> Favoritar</div>
-				</a>
+		<div class="w-2/3 mt-5 text-gray-400">
+			<div v-for="(comment, index) in posts">
+				<MessageItem
+					:index="index"
+					:msg="comment"
+					:toggle-favorite-post="toggleFavoritePost"
+				/>
 			</div>
 		</div>
 
---
+++ src/components/MessageItem.vue
@@ -0,0 +1,45 @@
+<script setup>
+import NotFavoriteIcon from './icons/IconNotFavorite.vue';
+import FavoriteIcon from './icons/IconFavorite.vue';
+</script>
+
+<script>
+export default {
+	props: ['msg', 'index', 'toggleFavoritePost'],
+};
+</script>
+
+<template>
+	<div class="flex mt-5 mb-2">
+		<div>
+			<img src="../assets/Avatar.png" />
+		</div>
+		<div class="w-full ml-4">
+			<div class="box-border bg-neutral-900 mb-2 py-5 px-2.5 rounded-lg">
+				<p>{{ msg.message }}</p>
+			</div>
+			<div class="ml-auto flex justify-end">
+				<a
+					href="#"
+					title="favoritar"
+					v-on:click="toggleFavoritePost(index)"
+				>
+					<div
+						class="flex items-center bg-neutral-900 px-4 py-1 rounded-md"
+						v-if="msg.isFavorite"
+					>
+						<span>Favorito</span>
+						<FavoriteIcon class="mt-auto ml-2" />
+					</div>
+					<div
+						class="flex items-center bg-neutral-900 px-4 py-1 rounded-md"
+						v-else
+					>
+						<span>Favoritar</span>
+						<NotFavoriteIcon class="mt-auto ml-2" />
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</template>
